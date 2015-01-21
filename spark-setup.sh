23ce6486fe8eb9e9cb8f5d1a850540e9c1de06c9
rightoverthere <whoisth@rightoverthere.com>
2015-01-20 21:53:16 -0800

    quick tweak for spark bin

--- spark-setup.sh
+++ spark-setup.sh
@@ -1,20 +1,16 @@
 #spark
 
-#http://d3kbcqa49mib13.cloudfront.net/spark-1.1.1-bin-hadoop2.4.tgz
+wget http://d3kbcqa49mib13.cloudfront.net/spark-1.1.1-bin-hadoop2.4.tgz
 
-wget http://www.us.apache.org/dist/spark/spark-1.1.1/spark-1.1.1.tgz
+#wget http://www.us.apache.org/dist/spark/spark-1.1.1/spark-1.1.1.tgz
 
-sudo tar -xzvf spark-1.1.1.tgz
+sudo tar -xzvf spark-1.1.1-bin-hadoop2.4
 
-sudo mv spark-1.1.1 /usr/local/
+sudo mv spark-1.1.1-bin-hadoop2.4 /usr/local/
 
 cd /usr/local
 
-ln -s spark-1.1.1/ spark
-
-cd spark
-
-mvn -Phadoop-2.6 -Dhadoop.version=2.6.0 -DskipTests clean package
+ln -s spark-1.1.1-bin-hadoop2.4/ spark
 
 echo "export SPARK_HOME=/usr/local/spark" | sudo tee -a /etc/bash.bashrc
 echo 'export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin' | sudo tee -a /etc/bash.bashrc 
